64a52bff6f83998f4640cdcfb8fe658bb0f123c3
Marjon <marjonsantiago49@gmail.com>
2023-05-22 19:21:26 -0400

    setup static assets

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+import os
+app = Flask(__name__,)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("test.html")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 8080)),
+        debug=True,
+    )
