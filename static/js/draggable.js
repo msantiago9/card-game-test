7f68ae7419ede90f88cc283f4337e72fe5f0c51d
Marjon <marjonsantiago49@gmail.com>
2023-05-22 20:38:19 -0400

    create draggable divs

--- static/js/draggable.js
+++ static/js/draggable.js
@@ -1,37 +1,41 @@
-var selected = null, // Object of the element to be moved
-  x_pos = 0,
-  y_pos = 0, // Stores x & y coordinates of the mouse pointer
-  x_elem = 0,
-  y_elem = 0; // Stores top, left values (edge) of the element
+var draggableElements = document.getElementsByClassName("draggable");
 
-// Will be called when user starts dragging an element
-function _drag_init(elem) {
-  // Store the object of the element which needs to be moved
-  selected = elem;
-  x_elem = x_pos - selected.offsetLeft;
-  y_elem = y_pos - selected.offsetTop;
+for (var i = 0; i < draggableElements.length; i++) {
+  dragElement(draggableElements[i]);
 }
 
-// Will be called when user dragging an element
-function _move_elem(e) {
-  x_pos = document.all ? window.event.clientX : e.pageX;
-  y_pos = document.all ? window.event.clientY : e.pageY;
-  if (selected !== null) {
-    selected.style.left = x_pos - x_elem + "px";
-    selected.style.top = y_pos - y_elem + "px";
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "-child")) {
+    document.getElementById(elmnt.id + "-child").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    pos3 = parseInt(e.clientX);
+    pos4 = parseInt(e.clientY);
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+    return false;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    pos1 = pos3 - parseInt(e.clientX);
+    pos2 = pos4 - parseInt(e.clientY);
+    pos3 = parseInt(e.clientX);
+    pos4 = parseInt(e.clientY);
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    // console.log(elmnt.offsetTop);
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
 }
-
-// Destroy the object when we are done
-function _destroy() {
-  selected = null;
-}
-
-// Bind the functions...
-document.getElementById("pokoteng-sleep").onmousedown = function () {
-  _drag_init(this);
-  return false;
-};
-
-document.onmousemove = _move_elem;
-document.onmouseup = _destroy;
